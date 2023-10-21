9d005b8ecffed78a90b6b9c7d8fb6a1964820962
Sarah Anja <100360306+macobac@users.noreply.github.com>
2023-10-21 12:45:34 +0200

    gg

--- js/auth/apiLinks.mjs
+++
@@ -1,8 +0,0 @@
-//alle api links skal skrives her og exporteres
-
-export const API_BASE_URL = 'https://api.noroff.dev/api/v1';
-
-export const API_REG_URL = `${API_BASE_URL}/social/auth/register`;
-
-export const API_LOGIN_URL = `${API_BASE_URL}/social/auth/login`;
-
