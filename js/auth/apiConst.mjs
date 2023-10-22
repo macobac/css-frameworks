2af3fa13f2309971c0b911f6c161587f1fedf8c8
Sarah Anja <100360306+macobac@users.noreply.github.com>
2023-10-22 12:02:48 +0200

    loginurl

--- js/auth/apiConst.mjs
+++ js/auth/apiConst.mjs
@@ -1,3 +1,5 @@
 export const apiBaseUrl = 'https://api.noroff.dev/api/v1';
 
 export const registerUrl = `${apiBaseUrl}/social/auth/register`;
+
+export const loginUrl = `${apiBaseUrl}/social/auth/login`;
