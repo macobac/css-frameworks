c3fb6b1fedb29d05363303f339fe93bb7895a613
Sarah Anja <100360306+macobac@users.noreply.github.com>
2023-10-22 15:21:26 +0200

    still idk

--- js/auth/login.mjs
+++ js/auth/login.mjs
@@ -1,10 +1,8 @@
 import { loginUrl } from "../auth/apiConst.mjs";
 import { userToLogin } from "../userData/userToLogin.mjs";
 import loginUser from "../auth/loginUser.mjs";
-import { loginEmail } from "../userData/formData.mjs";
 
-const loginBtn = document.querySelector('loginbtn');
+const loginBtn = document.querySelector('.loginbtn');
 
-loginEmail.addEventListener('input', function() {
-    console.log(loginEmail.value);
-})
+loginBtn.addEventListener('click', loginUser(loginUrl, userToLogin))
+
--- js/auth/register.mjs
+++ js/auth/register.mjs
@@ -2,7 +2,7 @@ import { registerUrl } from "../auth/apiConst.mjs";
 import { userToReg } from "../userData/userToReg.mjs";
 import registerUser from "../auth/registerUser.mjs";
 
-const regBtn = document.querySelector('regbtn');
+const regBtn = document.querySelector('.regbtn');
 
 regBtn.addEventListener('click', registerUser(registerUrl, userToReg));
 
