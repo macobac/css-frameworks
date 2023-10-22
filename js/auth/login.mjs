ea82b881da2a355b922f477757d9621c3723eae9
Sarah Anja <100360306+macobac@users.noreply.github.com>
2023-10-22 15:02:35 +0200

    fffffff

--- js/auth/login.mjs
+++ js/auth/login.mjs
@@ -1,7 +1,10 @@
 import { loginUrl } from "../auth/apiConst.mjs";
 import { userToLogin } from "../userData/userToLogin.mjs";
 import loginUser from "../auth/loginUser.mjs";
+import { loginEmail } from "../userData/formData.mjs";
 
 const loginBtn = document.querySelector('loginbtn');
 
-loginBtn.addEventListener('click', loginUser(loginUrl, userToLogin));
+loginBtn.addEventListener('click', function() {
+    console.log(loginEmail.value);
+})
--- js/userData/formData.mjs
+++ js/userData/formData.mjs
@@ -1,7 +1,7 @@
-export const regName = document.querySelector('#regusername').innerHTML;
-export const regEmail = document.querySelector('#regemail').innerHTML;
-export const regPassword = document.querySelector('#regpassword').innerHTML;
+export const regName = document.querySelector('#regusername');
+export const regEmail = document.querySelector('#regemail');
+export const regPassword = document.querySelector('#regpassword');
 
-export const loginEmail = document.querySelector('#useremail').innerHTML;
-export const loginPassword = document.querySelector('#loginpassword').innerHTML;
+export const loginEmail = document.querySelector('#useremail');
+export const loginPassword = document.querySelector('#loginpassword');
 
