0b97f703f23afac0e1c75d597ed52e31bcecf4dc
Sarah Anja <100360306+macobac@users.noreply.github.com>
2023-11-06 11:19:13 +0100

    made successMsf unct

---
+++ js/auth/successMessage.mjs
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @param {*} btn 
+ * @param {string} successMsg 
+ * ```js
+ * successMessage(btn, successMsg);
+ * ``` 
+ */
+export default function successMessage(btn, successMsg) {
+    const successDiv = document.createElement('div');
+    successDiv.className = 'success-message';
+    successDiv.textContent = successMsg;
+    successDiv.style.color = 'green';
+
+    btn.parentNode.insertBefore(successDiv, btn.nextSibling);
+}
