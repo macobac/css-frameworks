3cc4cf9ca623b117d716d6ad810fdee90ff52cf4
Sarah Anja <100360306+macobac@users.noreply.github.com>
2023-11-14 13:39:56 +0100

    imported consts

--- js/userData/userToLogin.mjs
+++ js/userData/userToLogin.mjs
@@ -1,5 +1,7 @@
-export default function userToLogin() {
+import { loginEmail } from "../auth/constants.mjs";
+import { loginPassword } from "../auth/constants.mjs";
 
+export default function userToLogin() {
     const user = {
         email: loginEmail.value,
         password: loginPassword.value
