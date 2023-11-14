6e59e71fb160ad397129ed99ea0d355f7933a1f3
Sarah Anja <100360306+macobac@users.noreply.github.com>
2023-11-14 14:05:31 +0100

    imported and added ClearMsg

--- js/auth/login.mjs
+++ js/auth/login.mjs
@@ -1,11 +1,13 @@
 import { loginUrl } from "../auth/constants.mjs";
 import userToLogin from "../userData/userToLogin.mjs";
 import loginUser from "../auth/loginUser.mjs";
-
-const loginForm = document.querySelector('#loginform');
+import { loginForm } from "../auth/constants.mjs";
+import { loginBtn } from "../auth/constants.mjs";
+import clearMessage from "../auth/clearMessage.mjs";
 
 loginForm.addEventListener('submit', async (event) => {
     event.preventDefault();
+    clearMessage(loginBtn, 'error-message');
     const userObj = userToLogin();
     loginUser(loginUrl, userObj);
 })
