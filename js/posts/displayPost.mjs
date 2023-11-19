b7aa3f7f4e6239cea595860024c618629598262c
Sarah Anja <100360306+macobac@users.noreply.github.com>
2023-11-19 14:18:22 +0100

    added img to function

--- js/posts/displayPost.mjs
+++ js/posts/displayPost.mjs
@@ -4,9 +4,12 @@ export default async function displayPost(posts) {
     newTitle.innerText = posts.title;
     const newBody = document.createElement("p");
     newBody.innerText = posts.body;
+    //const newImg = document.createElement("img");
+    //newImg.src = posts.media;
 
     newDiv.appendChild(newTitle);
     newDiv.appendChild(newBody);
+    //newDiv.appendChild(newImg);
 
     const currentDiv = document.querySelector(".fetchPostsDiv");
     document.body.insertBefore(newDiv, currentDiv);
