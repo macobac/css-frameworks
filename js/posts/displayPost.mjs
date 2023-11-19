76830c46d6fc510258910376639b9434140db554
Sarah Anja <100360306+macobac@users.noreply.github.com>
2023-11-19 15:25:25 +0100

    deleted authordiv

--- js/posts/displayPost.mjs
+++ js/posts/displayPost.mjs
@@ -2,8 +2,6 @@ import { fetchedDiv } from "../auth/constants.mjs";
 
 export default async function displayPost(posts) {
     const newDiv = document.createElement("div");
-    const newAuthor = document.createElement("p");
-    newAuthor.innerText = posts.author;
     const newTitle = document.createElement("h2");
     newTitle.innerText = posts.title;
     const newBody = document.createElement("p");
@@ -11,10 +9,8 @@ export default async function displayPost(posts) {
     //const newImg = document.createElement("img");
     //newImg.src = posts.media;
 
-    newDiv.appendChild(newAuthor);
     newDiv.appendChild(newTitle);
     newDiv.appendChild(newBody);
-
     //newDiv.appendChild(newImg);
 
     document.body.insertBefore(newDiv, fetchedDiv);
