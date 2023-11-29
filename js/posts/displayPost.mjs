b9605987b776fcf71cf65614183b6b2d7511c95e
Sarah Anja <100360306+macobac@users.noreply.github.com>
2023-11-29 15:27:01 +0100

    added some bootstrap justasjons

--- js/posts/displayPost.mjs
+++ js/posts/displayPost.mjs
@@ -2,20 +2,29 @@ import { fetchedDiv } from "../auth/constants.mjs";
 
 export default async function displayPost(posts) {
     const newDiv = document.createElement("div");
+    newDiv.classList.add("card");
+    newDiv.style.width = "12rem";
+    const innerDiv = document.createElement("div");
+    innerDiv.classList.add("card-body");
     const newTitle = document.createElement("h2");
     newTitle.innerText = posts.title;
+    newTitle.classList.add("card-title");
     const newBody = document.createElement("p");
     newBody.innerText = posts.body;
+    newBody.classList.add("card-text")
+    //img can only be added if there is img. needs to fix
     //const newImg = document.createElement("img");
     //newImg.src = posts.media;
+    //newImg.classList.add("card-img-top");
     const newAuthor = document.createElement("p");
     newAuthor.innerText = posts.author.name;
-    
-    newDiv.appendChild(newAuthor);
-    newDiv.appendChild(newTitle);
-    newDiv.appendChild(newBody);
-    //newDiv.appendChild(newImg);
-    
 
-    document.body.insertBefore(newDiv, fetchedDiv);
+    //innerDiv.appendChild(newImg);
+    innerDiv.appendChild(newAuthor);
+    innerDiv.appendChild(newTitle);
+    innerDiv.appendChild(newBody);
+    
+    newDiv.appendChild(innerDiv);
+
+    fetchedDiv.appendChild(newDiv);
 }
