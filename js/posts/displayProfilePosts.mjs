5dae217f5b61594a2a0dd4491f10cef9f44b1e23
Sarah Anja <100360306+macobac@users.noreply.github.com>
2023-12-05 14:28:45 +0100

    d

---
+++ js/posts/displayProfilePosts.mjs
@@ -0,0 +1,47 @@
+import { fetchedDiv } from "../auth/constants.mjs";
+
+import formatDate from "../posts/formatDate.mjs";
+
+export default async function displayPost(posts) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("card", "my-3", "mx-auto");
+    
+    const innerDiv = document.createElement("div");
+    innerDiv.classList.add("card-body");
+    const newTitle = document.createElement("h2");
+    newTitle.innerText = posts.title;
+    newTitle.classList.add("card-title");
+    const newBody = document.createElement("p");
+    newBody.innerText = posts.body;
+    newBody.classList.add("card-text");
+    const newDate = document.createElement("p");
+    newDate.innerText = formatDate(posts.created);
+    newDate.classList.add("postCreatedOn");
+    const newAuthor = document.createElement("a");
+    newAuthor.innerText = posts.name;
+    newAuthor.href = `profile.html?name=${posts.name}`; 
+    newAuthor.classList.add("card-text", "post-author");
+    newAuthor.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.location.href = newAuthor.href;
+    });
+
+    let newImg;
+    if (posts.media) {
+        newImg = document.createElement("img");
+        newImg.src = posts.media;
+        newImg.classList.add("card-img-top");
+    };
+    if (newImg) {
+        innerDiv.appendChild(newImg);
+    };
+
+    innerDiv.appendChild(newAuthor);
+    innerDiv.appendChild(newDate);
+    innerDiv.appendChild(newTitle);
+    innerDiv.appendChild(newBody);
+    
+    newDiv.appendChild(innerDiv);
+
+    fetchedDiv.appendChild(newDiv);
+}
