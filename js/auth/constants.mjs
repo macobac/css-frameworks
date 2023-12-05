455c4f88e31d72edda126282cf68241e5c0620d4
Sarah Anja <100360306+macobac@users.noreply.github.com>
2023-12-05 14:13:17 +0100

    added profile consts

--- js/auth/constants.mjs
+++ js/auth/constants.mjs
@@ -2,6 +2,7 @@ export const apiBaseUrl = 'https://api.noroff.dev/api/v1';
 export const registerUrl = `${apiBaseUrl}/social/auth/register`;
 export const loginUrl = `${apiBaseUrl}/social/auth/login`;
 export const postsUrl = `${apiBaseUrl}/social/posts`;
+export const profilesUrl = `${apiBaseUrl}/social/profiles`;
 
 export const regBtn = document.querySelector(".regbtn");
 export const regForm = document.querySelector("#regform");
@@ -24,3 +25,7 @@ export const fetchedDiv = document.querySelector(".fetchPostsDiv");
 
 export const latestPosts = document.querySelector(".latest-posts")
 export const oldestPosts = document.querySelector(".oldest-posts");
+
+export const profileName = document.querySelector(".profile-name")
+export const profileFollowing = document.querySelector(".profile-following");
+export const profileFollowers = document.querySelector(".profile-followers");
