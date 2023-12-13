e9012784a8719092f7c2218a52b0f877fe73ab41
Sarah Anja <100360306+macobac@users.noreply.github.com>
2023-12-13 18:27:11 +0100

    deleted async word

--- js/posts/redirectToNewPost.mjs
+++ js/posts/redirectToNewPost.mjs
@@ -1,7 +1,7 @@
 import { newPostBtn } from "../auth/constants.mjs";
 
 export default function redirectToNewPost() {
-    newPostBtn.addEventListener("click", async (e) => {
+    newPostBtn.addEventListener("click", (e) => {
         e.preventDefault();
         window.location.href = "/newPost.html";
     });
