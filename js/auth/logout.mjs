ed7fc52238d23b15e7feec694c4fd22c7653f8c5
Sarah Anja <100360306+macobac@users.noreply.github.com>
2023-12-13 19:11:40 +0100

    remove currentusername when logout

--- js/auth/logout.mjs
+++ js/auth/logout.mjs
@@ -3,6 +3,7 @@ import { logoutBtn } from "./constants.mjs";
 export default function logout() {
     logoutBtn.addEventListener('click', () => {
         localStorage.removeItem('accessToken');
+        localStorage.removeItem('currentUserName');
         window.location.href = '/index.html';
     });
 }
