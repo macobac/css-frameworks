b005e0160583491399e0d8da1d5ce2894367eca0
Sarah Anja <100360306+macobac@users.noreply.github.com>
2023-12-15 13:24:18 +0100

    changed function to take parameter userid, added del and upd btns

--- js/posts/displayProfilePosts.mjs
+++ js/posts/displayProfilePosts.mjs
@@ -5,7 +5,7 @@ import formatDate from "../posts/formatDate.mjs";
  * 
  * @param {any} posts 
  */
-export default async function displayPost(posts) {
+export default async function displayProfilePosts(posts, authorname) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("card", "my-3", "mx-auto");
     const innerDiv = document.createElement("div");
@@ -13,9 +13,16 @@ export default async function displayPost(posts) {
     const newTitle = document.createElement("h2");
     newTitle.innerText = posts.title;
     newTitle.classList.add("card-title");
+    newTitle.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.location.href = `singlePost.html?id=${posts.id}`;
+    })
     const newBody = document.createElement("p");
     newBody.innerText = posts.body;
     newBody.classList.add("card-text");
+    const newAuthor = document.createElement("p");
+    newAuthor.innerText = authorname; 
+    newAuthor.classList.add("card-text", "post-author", "text-primary");
     const newDate = document.createElement("p");
     newDate.innerText = formatDate(posts.created);
     newDate.classList.add("postCreatedOn");
@@ -28,9 +35,21 @@ export default async function displayPost(posts) {
     if (newImg) {
         innerDiv.appendChild(newImg);
     };
+    innerDiv.appendChild(newAuthor);
     innerDiv.appendChild(newDate);
     innerDiv.appendChild(newTitle);
     innerDiv.appendChild(newBody);
     newDiv.appendChild(innerDiv);
     fetchedDiv.appendChild(newDiv);
+
+    const currentUser = localStorage.getItem('currentUserName');
+    if (authorname === currentUser) {
+        const updateBtn = document.createElement("button");
+        updateBtn.innerText = `Update post`;
+        innerDiv.appendChild(updateBtn);
+        const deleteBtn = document.createElement("button");
+        deleteBtn.innerText = `Delete post`;
+        innerDiv.appendChild(deleteBtn);
+
+}
 }
